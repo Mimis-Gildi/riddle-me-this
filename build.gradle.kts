faa605cff19ea9b731592c94f536bfe97a158564
R!dd13r <12781006+rdd13r@users.noreply.github.com>
2026-03-31 11:44:52 -0400

    Cleaning out build logic.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,8 @@
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
-import me.riddle.the.dependencies.DependencyUpdatePolicy
-import me.riddle.the.dependencies.LegacyOutdatedDependenciesProcessor
-import me.riddle.the.dependencies.shouldUseReleaseOnlyDependencies
-import me.riddle.the.tasks.registerVerifyToolchainTask
 import org.asciidoctor.gradle.jvm.AsciidoctorTask
 import org.asciidoctor.gradle.jvm.epub.AsciidoctorEpubTask
 import org.asciidoctor.gradle.jvm.epub.AsciidoctorEpubTask.EPUB3
 import org.asciidoctor.gradle.jvm.pdf.AsciidoctorPdfTask
 import org.slf4j.LoggerFactory
-import java.util.*
 
 val useJavaVersion: String by project
 val resumeFolder = file(project.property("resume.root.folder") as String)
@@ -20,7 +14,7 @@ val resumeVersion: String by project
 val pdfResumeThemeIds = listOf(
     "conservative-resume",
     "creative-resume",
-    "technical-resume",
+    "engineering-resume",
     "core-resume"
     )
 
@@ -28,10 +22,6 @@ private val log by lazy { LoggerFactory.getLogger("me.riddle.the.build") }
 
 plugins {
     `kotlin-dsl`
-
-    alias(libs.plugins.ben.manes)
-    alias(libs.plugins.dokka)
-
     alias(libs.plugins.asciidoctor.jvm.pdf)
     alias(libs.plugins.asciidoctor.jvm.gems)
     alias(libs.plugins.asciidoctor.jvm.epub)
@@ -53,16 +43,10 @@ java {
 }
 
 dependencies {
-    implementation(gradleApi())
-    implementation(platform(kotlin("bom")))
-
     api(libs.slf4j.api)
     implementation(libs.kotlin.logging)
     implementation(libs.logback.classic)
 }
-
-registerVerifyToolchainTask()
-
 
 tasks.named<Jar>("jar") {
     log.info("\t|=> Riddle me that Jar Task is used as a dependency here, and thus explicitly disabled!")
@@ -78,35 +62,11 @@ tasks.named<AsciidoctorEpubTask>("asciidoctorEpub") { configureAsciiDocInput(thi
 pdfThemes {
     pdfResumeThemeIds.forEach { themeId ->
         local(themeId) {
-            themeDir = file("resources/themes")
+            themeDir = file(resumeFolder)
             themeName = themeId
         }
     }
 }
-
-/**
- * Configures the dependency update task to be run on build at will.
- */
-tasks.named<DependencyUpdatesTask>(DependencyUpdatePolicy.TASK_NAME).configure {
-    checkForGradleUpdate = DependencyUpdatePolicy.CHECK_GRADLE
-    outputFormatter = DependencyUpdatePolicy.FORMAT
-    outputDir = DependencyUpdatePolicy.FOLDER
-    reportfileName = DependencyUpdatePolicy.FILE_NAME
-
-    rejectVersionIf { shouldUseReleaseOnlyDependencies(log) && isStableVersion(candidate.version).not() }
-}
-
-/**
- * Triggers Ben's dependency update process and extracts suggestions for outdated dependencies.
- */
-tasks.register(DependencyUpdatePolicy.PROCESS_NAME) {
-    dependsOn(DependencyUpdatePolicy.TASK_NAME)
-
-    doLast { LegacyOutdatedDependenciesProcessor(file(DependencyUpdatePolicy.FILE_PATH), log).printDependencyUpdateReport() }
-}
-
-tasks.named(DependencyUpdatePolicy.PROCESS_NAME) { outputs.upToDateWhen { false } }
-
 
 /**
  * Configures the Asciidoctor task to generate documents from a specified source directory
@@ -139,21 +99,4 @@ fun configureAsciiDocInput(
             )
         )
     }
-}
-
-/**
- * Returns true if the given version string is considered stable.
- *
- * A stable version is one that contains the string "RELEASE", "FINAL", or "GA" (case-insensitive), or matches the
- * regular expression "^[0-9,.v-]+(-r)?$". The regular expression allows for versions with numbers, commas,
- * periods, "v" characters, hyphens, and optional "-r" suffixes.
- *
- * @param version the version string to check
- * @return true if the version is stable, false otherwise
- */
-fun isStableVersion(version: String): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase(Locale.getDefault()).contains(it) }
-    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-    val isStable = stableKeyword || regex.matches(version)
-    return isStable
 }
